ed4c073246514f7e3f12c790c627de7feba1d89a
tuanpv1 <tuantrylook@gmail.com>
2017-03-04 13:58:06 +0700

    thay doi tieu de

--- frontend/views/header/header.php
+++ frontend/views/header/header.php
@@ -92,7 +92,7 @@ use yii\helpers\Url;
     <div id="vingroup_line_container">
         <div id="vingroup_line">
             <div id="vingroup_line__left">
-                Trang thông tin tham tử 3s
+                Trang thông tin Thám Tử 3s
             </div>
 <!--            <div id="vingroup_line__right">-->
 <!--                <div id="vingroup_line__right__dropdown">-->
