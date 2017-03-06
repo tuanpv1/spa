41344f8420a6655e48dde85665e8c543ae5bc853
tuanpv1 <tuantrylook@gmail.com>
2017-03-06 16:15:31 +0700

    fix loi xoa file

--- backend/controllers/BannerController.php
+++ backend/controllers/BannerController.php
@@ -116,7 +116,7 @@ class BannerController extends Controller
             $image = UploadedFile::getInstance($model, 'image');
             if ($image) {
                 $file_name = Yii::$app->user->id . '.' . uniqid() . time() . '.' . $image->extension;
-                $tmp = Yii::getAlias('@backend') . '/web/' . Yii::getAlias('@image_affiliate_company') . '/';
+                $tmp = Yii::getAlias('@web') . "/" . Yii::getAlias('@image_banner') . "/" ;
                 if ($image->saveAs($tmp . $file_name)) {
                     unlink($tmp.$image_old);
                     $model->image = $file_name;
@@ -146,7 +146,7 @@ class BannerController extends Controller
     public function actionDelete($id)
     {
         $model = $this->findModel($id);
-        $tmp = Yii::getAlias('@backend') . '/web/' . Yii::getAlias('@image_affiliate_company') . '/';
+        $tmp = Yii::getAlias('@web') . "/" . Yii::getAlias('@image_banner') . "/" ;
         unlink($tmp.$model->image);
         $model->delete();
         Yii::$app->getSession()->setFlash('success', 'Xóa banner thành công');
