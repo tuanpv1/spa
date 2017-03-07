6d075e9fcdcc57e75bde37e653f2aea29163d7e8
tuanpv1 <tuantrylook@gmail.com>
2017-03-07 14:03:45 +0700

    df

--- frontend/views/header/header.php
+++ frontend/views/header/header.php
@@ -92,7 +92,7 @@ use yii\helpers\Url;
     <div id="vingroup_line_container">
         <div id="vingroup_line">
             <div id="vingroup_line__left">
-                Trang thông tin Thám Tử 3s
+                Trang thông tin Thám Tử VIP
             </div>
 <!--            <div id="vingroup_line__right">-->
 <!--                <div id="vingroup_line__right__dropdown">-->
