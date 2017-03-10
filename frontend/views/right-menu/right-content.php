52c3382486e14de6e0303e09da509c446332ddb0
tuanpv1 <tuantrylook@gmail.com>
2017-03-10 18:45:23 +0700

    add

---
+++ frontend/views/right-menu/right-content.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TuanPham
+ * Date: 3/9/2017
+ * Time: 2:23 PM
+ */
+?>
+<div class="grid4 main-news-sidebar-left">
+    <p class="segoeui">Bài viết nổi bật</p>
+    <ul>
+        <?php if(isset($listNewsMoi) && !empty($listNewsMoi)){ foreach($listNewsMoi as $item){ /** @var \common\models\News $item */?>
+        <li>
+            <a href="<?= \yii\helpers\Url::to(['site/detail-news','id'=>$item->id]) ?>">
+                <img width="150" height="150" src="<?= $item->getImage() ?>" class="attachment-thumbnail size-thumbnail wp-post-image" alt="<?= $item->title ?>" />
+            </a>
+            <div class="main-news-thumb">
+                <a href="<?= \yii\helpers\Url::to(['site/detail-news','id'=>$item->id]) ?>"><?= $item->title ?></a>
+                <time><img src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/images/icons/news-icon-time.gif" alt="#"><span><?= date('dd-mm-Y',$item->created_at) ?></span></time>
+            </div>
+        </li>
+        <?php }}else{?>
+        <li>Đang cập nhật</li>
+        <?php } ?>
+    </ul>
+</div>
