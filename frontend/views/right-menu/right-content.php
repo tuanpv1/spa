adf663c1f7020ccc004c0b29146f90ccf7d914d3
tuanpv1 <tuantrylook@gmail.com>
2017-03-10 19:11:41 +0700

    a

--- frontend/views/right-menu/right-content.php
+++ frontend/views/right-menu/right-content.php
@@ -16,7 +16,7 @@
             </a>
             <div class="main-news-thumb">
                 <a href="<?= \yii\helpers\Url::to(['site/detail-news','id'=>$item->id]) ?>"><?= $item->title ?></a>
-                <time><img src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/images/icons/news-icon-time.gif" alt="#"><span><?= date('dd-mm-Y',$item->created_at) ?></span></time>
+                <time><img src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/images/icons/news-icon-time.gif" alt="#"><span><?= date('d-m-Y',$item->created_at) ?></span></time>
             </div>
         </li>
         <?php }}else{?>
