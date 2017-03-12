490335a99dfab3c5ae42df00186667479648c911
tuanpv1 <tuantrylook@gmail.com>
2017-03-12 16:57:00 +0700

    a

--- frontend/views/right-menu/right-content.php
+++ frontend/views/right-menu/right-content.php
@@ -11,9 +11,7 @@
     <ul>
         <?php if(isset($listArray) && !empty($listArray)){ foreach($listArray as $item){ /** @var \common\models\AffiliateCompany $item */?>
         <li>
-            <span>
-                <a href="<?= \yii\helpers\Url::to(['site/detail-news','id'=>$item->id]) ?>">Tin tức <?= strtolower($item->name) ?></a>
-            </span>
+            <a href="<?= \yii\helpers\Url::to(['site/detail-news','id'=>$item->id]) ?>">Tin tức <?= strtolower($item->name) ?></a><br>
         </li>
         <?php }}else{?>
         <li>Đang cập nhật</li>
