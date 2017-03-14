f9aaefcb3b51c19126a8bc581405f83ab3446b02
tuanpv1 <tuantrylook@gmail.com>
2017-03-14 13:32:07 +0700

    a

--- frontend/views/footer/render-list-new.php
+++ frontend/views/footer/render-list-new.php
@@ -8,32 +8,29 @@
 use yii\helpers\Url;
 /** @var \common\models\News $model */
 ?>
-<div class="main-benef main-section">
-    <div id="main_lidt_<?= $model->id ?>"  class="main-title tac ttu">
+<div id="main_da"></div>
+<div  class="main-project main-section">
+    <div class="main-title tac ttu">
         <span class="segoeui">Thám Tử VIP</span>
         <h2 class="utm-trajan">Tin tức <?= $model?$model->title:'' ?></h2>
     </div>
-    <div class="container">
-        <?php if (isset($listNews) && !empty($listNews)) {
-            foreach ($listNews as $item) {
-                /** @var $item \common\models\News */
-                ?>
-                <div class="grid3">
-                    <a class="text-center" href="javascript:;" rel="nofollow">
-                        <img style="width: 90%;height: 250px" src="<?= $item->getImage() ?>"
-                             alt="<?= $item->title ?>"/>
-                    </a>
-                    <div class="benef--box-title">
-                        <a href="<?= Url::to(['site/detail-news','id'=>$item->id])?>"><?= $item->title ?></a>
-                    </div>
-                    <div class="benef-box-content">
-                        <?= \common\helpers\CUtils::subString(trim($item->short_description), 300) ?>
-                    </div>
-                </div>
-            <?php }
-        } ?>
-    </div>
-    <div class="grid12 tac view-more-page">
-        <a href="<?= Url::toRoute(['site/news','id'=>$model->id]) ?>" class="view-more HelveticaiDesignVnlt ttu">Xem thêm<span></span></a>
+    <div class="container ovfh">
+        <ul class="main-project-list fluid">
+            <?php if(isset($listNews) && !empty($listNews)){
+                foreach($listNews as $item){
+                    /** @var $item \common\models\News */
+                    ?>
+                    <li class="grid3 wow fadeInUp" data-wow-delay=".5s">
+                        <img style="height: 400px" src="<?= $item->getImage() ?>" alt="#">
+                        <?php if(!empty($item->title)){ ?>
+                            <a href="<?= Url::to(['site/detail-news','id'=>$item->id]) ?>"><?= $item->title ?></a>
+                        <?php } ?>
+                        <div class="benef-box-content">
+                            <?= \common\helpers\CUtils::subString(trim($item->short_description), 300) ?>
+                        </div>
+                    </li>
+                <?php }
+            } ?>
+        </ul>
     </div>
 </div>
