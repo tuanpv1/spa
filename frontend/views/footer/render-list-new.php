ed702056d2dc5692016f377e32f4c046607390a3
tuanpv1 <tuantrylook@gmail.com>
2017-03-14 13:41:10 +0700

    s

--- frontend/views/footer/render-list-new.php
+++ frontend/views/footer/render-list-new.php
@@ -20,13 +20,13 @@ use yii\helpers\Url;
                     /** @var $item \common\models\News */
                     ?>
                     <li class="grid3 wow fadeInUp" data-wow-delay=".5s">
-                        <img style="height: 400px" src="<?= $item->getImage() ?>" alt="#">
-                        <?php if(!empty($item->title)){ ?>
-                            <a href="<?= Url::to(['site/detail-news','id'=>$item->id]) ?>"><?= $item->title ?></a>
-                        <?php } ?>
-                        <div class="benef-box-content">
-                            <?= \common\helpers\CUtils::subString(trim($item->short_description), 300) ?>
-                        </div>
+                        <img style="height: 200px" src="<?= $item->getImage() ?>" alt="#">
+                        <a href="<?= Url::to(['site/detail-news','id'=>$item->id]) ?>">
+                            <?= $item->title ?>
+                            <div class="benef-box-content">
+                                <?= \common\helpers\CUtils::subString(trim($item->short_description), 300) ?>
+                            </div>
+                        </a>
                     </li>
                 <?php }
             } ?>
