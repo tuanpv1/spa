cb323a61d9711ff7ff61fa1cf89c30964e10ab61
tuanpv1 <tuantrylook@gmail.com>
2017-03-14 09:11:22 +0700

    sa

--- frontend/views/footer/render-list-new.php
+++ frontend/views/footer/render-list-new.php
@@ -8,40 +8,6 @@
 use yii\helpers\Url;
 /** @var \common\models\News $model */
 ?>
-<div class="col-md-12 col">
-    <div  class="main-title tac ttu">
-        <span class="segoeui">Thám Tử VIP</span>
-        <h2 class="utm-trajan">Tin tức <?= $model?$model->title:'' ?></h2>
-    </div>
-    <div class="container">
-        <?php if (isset($listNews) && !empty($listNews)) {
-            foreach ($listNews as $item) {
-                /** @var $item \common\models\News */
-                ?>
-                <div class="item col-md-4">
-                    <div class="main-benef--box">
-                        <div class="benef--box-img">
-                            <a href="javascript:;" rel="nofollow">
-                                <img width="96" height="96" src="<?= $item->getImage() ?>"
-                                     class="attachment-post-thumbnail size-post-thumbnail wp-post-image"
-                                     alt="<?= $item->title ?>" srcset="" sizes="(max-width: 96px) 100vw, 96px"/>
-                            </a>
-                        </div>
-                        <div class="benef--box-title">
-                            <a href="<?= Url::to(['site/detail-news','id'=>$item->id])?>"><?= $item->title ?></a>
-                        </div>
-                        <div class="benef-box-content">
-                            <?= \common\helpers\CUtils::subString(trim($item->short_description), 300) ?>
-                        </div>
-                    </div>
-                </div>
-            <?php }
-        } ?>
-    </div>
-    <div class="tac view-more-page">
-        <a href="<?= Url::toRoute(['site/news','id'=>$model->id]) ?>" class="view-more HelveticaiDesignVnlt ttu">Xem thêm<span></span></a>
-    </div>
-</div>
 
 <div class="row">
     <div class="container">
