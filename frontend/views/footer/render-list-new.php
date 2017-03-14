5ddb3b50072f4477e12ba9452359dfd4e75f9ad5
tuanpv1 <tuantrylook@gmail.com>
2017-03-14 11:23:37 +0700

    a

--- frontend/views/footer/render-list-new.php
+++ frontend/views/footer/render-list-new.php
@@ -8,7 +8,7 @@
 use yii\helpers\Url;
 /** @var \common\models\News $model */
 ?>
-<div id="main_about" class="main-invest ovfh">
+<div class="main-benef main-section">
     <div id="main_lidt_<?= $model->id ?>"  class="main-title tac ttu">
         <span class="segoeui">Thám Tử VIP</span>
         <h2 class="utm-trajan">Tin tức <?= $model?$model->title:'' ?></h2>
@@ -19,28 +19,24 @@ use yii\helpers\Url;
                 /** @var $item \common\models\News */
                 ?>
                 <div class="grid3">
-                    <div class="posr">
-                        <div class="main-benef--box">
-                            <div class="benef--box-img">
-                                <a href="javascript:;" rel="nofollow">
-                                    <img width="96" height="96" src="<?= $item->getImage() ?>"
-                                         class="attachment-post-thumbnail size-post-thumbnail wp-post-image"
-                                         alt="<?= $item->title ?>" srcset="" sizes="(max-width: 96px) 100vw, 96px"/>
-                                </a>
-                            </div>
-                            <div class="benef--box-title">
-                                <a href="<?= Url::to(['site/detail-news','id'=>$item->id])?>"><?= $item->title ?></a>
-                            </div>
-                            <div class="benef-box-content">
-                                <?= \common\helpers\CUtils::subString(trim($item->short_description), 300) ?>
-                            </div>
-                        </div>
+                    <div class="benef--box-img">
+                        <a href="javascript:;" rel="nofollow">
+                            <img width="96" height="96" src="<?= $item->getImage() ?>"
+                                 class="attachment-post-thumbnail size-post-thumbnail wp-post-image"
+                                 alt="<?= $item->title ?>" srcset="" sizes="(max-width: 96px) 100vw, 96px"/>
+                        </a>
+                    </div>
+                    <div class="benef--box-title">
+                        <a href="<?= Url::to(['site/detail-news','id'=>$item->id])?>"><?= $item->title ?></a>
+                    </div>
+                    <div class="benef-box-content">
+                        <?= \common\helpers\CUtils::subString(trim($item->short_description), 300) ?>
                     </div>
                 </div>
             <?php }
         } ?>
     </div>
-    <div class="tac view-more-page container">
+    <div class="tac view-more-page">
         <a href="<?= Url::toRoute(['site/news','id'=>$model->id]) ?>" class="view-more HelveticaiDesignVnlt ttu">Xem thêm<span></span></a>
     </div>
 </div>
