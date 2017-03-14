c42c070c636cba07f3ab8c0dc63fd55bf5bf62a8
tuanpv1 <tuantrylook@gmail.com>
2017-03-14 09:30:07 +0700

    a

--- frontend/views/footer/render-list-new.php
+++ frontend/views/footer/render-list-new.php
@@ -14,36 +14,34 @@ use yii\helpers\Url;
         <span class="segoeui">Thám Tử VIP</span>
         <h2 class="utm-trajan">Tin tức <?= $model?$model->title:'' ?></h2>
     </div>
-    <div>
-        <div class="container">
-            <ul class="list-agency">
-                <?php if (isset($listNews) && !empty($listNews)) {
-                    foreach ($listNews as $item) {
-                        /** @var $item \common\models\News */
-                        ?>
-                        <li class="grid3">
-                            <div class="main-benef--box">
-                                <div class="benef--box-img">
-                                    <a href="javascript:;" rel="nofollow">
-                                        <img width="96" height="96" src="<?= $item->getImage() ?>"
-                                             class="attachment-post-thumbnail size-post-thumbnail wp-post-image"
-                                             alt="<?= $item->title ?>" srcset="" sizes="(max-width: 96px) 100vw, 96px"/>
-                                    </a>
-                                </div>
-                                <div class="benef--box-title">
-                                    <a href="<?= Url::to(['site/detail-news','id'=>$item->id])?>"><?= $item->title ?></a>
-                                </div>
-                                <div class="benef-box-content">
-                                    <?= \common\helpers\CUtils::subString(trim($item->short_description), 300) ?>
-                                </div>
+    <div class="container">
+        <ul class="list-agency">
+            <?php if (isset($listNews) && !empty($listNews)) {
+                foreach ($listNews as $item) {
+                    /** @var $item \common\models\News */
+                    ?>
+                    <li class="grid3">
+                        <div class="main-benef--box">
+                            <div class="benef--box-img">
+                                <a href="javascript:;" rel="nofollow">
+                                    <img width="96" height="96" src="<?= $item->getImage() ?>"
+                                         class="attachment-post-thumbnail size-post-thumbnail wp-post-image"
+                                         alt="<?= $item->title ?>" srcset="" sizes="(max-width: 96px) 100vw, 96px"/>
+                                </a>
                             </div>
-                        </li>
-                    <?php }
-                } ?>
-            </ul>
-        </div>
+                            <div class="benef--box-title">
+                                <a href="<?= Url::to(['site/detail-news','id'=>$item->id])?>"><?= $item->title ?></a>
+                            </div>
+                            <div class="benef-box-content">
+                                <?= \common\helpers\CUtils::subString(trim($item->short_description), 300) ?>
+                            </div>
+                        </div>
+                    </li>
+                <?php }
+            } ?>
+        </ul>
     </div>
-    <div class="main-section">
+    <div class="container">
         <div class="tac view-more-page">
             <a href="<?= Url::toRoute(['site/news','id'=>$model->id]) ?>" class="view-more HelveticaiDesignVnlt ttu">Xem thêm<span></span></a>
         </div>
