72faa6d812b7235cb4a2778757819283f3a6b426
tuanpv1 <tuantrylook@gmail.com>
2017-03-15 19:47:07 +0700

    a

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -35,12 +35,7 @@ $this->title = 'Thám tử VIP';
                     </div>
                 </div>
                 <div class="grid8">
-                    <p>
-                        <span class="wow fadeInLeft"
-                              data-wow-duration="2s"><?= \common\helpers\CUtils::subString1($gioithieu->title, 50) ?></span>
-                        <span class="tar wow fadeInLeft" data-wow-duration="2s"
-                              data-wow-delay=".3s"><?= substr($gioithieu->title, strlen(\common\helpers\CUtils::subString1($gioithieu->title, 50)) + 1) ?></span>
-                    </p>
+                    <p><span style="font-size: medium" class="wow fadeInLeft" data-wow-duration="2s"><?= $gioithieu->title ?><span></p>
                     <p class="segoeuil wow fadeIn segoeui taj" data-wow-duratioon="1s"
                        data-wow-delay="1s"><?= $gioithieu->short_description ?></p>
                 </div>
