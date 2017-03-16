c99e2e63d4cf6acdc503e2fdd2e93a789f501a51
tuanpv1 <tuantrylook@gmail.com>
2017-03-16 10:08:30 +0700

    a

--- frontend/views/site/_news.php
+++ frontend/views/site/_news.php
@@ -26,9 +26,9 @@ use yii\helpers\Url;
                          sizes="(max-width: 300px) 100vw, 300px"/> </a>
                 <div class="main-news-thumb">
                     <a href="<?= Url::toRoute(['detail-news', 'id' => $item->id]) ?>"><?= $item->title ?></a>
-                    <time><img
-                            src="http://vinpearl-condotel.vn/wp-content/themes/vinpearlcondotel/img/news-icon-time.gif"
-                            alt="#"><span><?= date('d/m/Y', $item->created_at) ?></span></time>
+                    <time><img style="width: 18px"
+                               src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/images/icons/news-icon-time.gif"
+                               alt="#"><span><?= date('d-m-Y', $item->created_at) ?></span></time>
                     <p><?= $item->short_description?$item->short_description:'Đang cập nhật ...' ?></p>
                 </div>
             </li>
