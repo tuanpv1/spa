709a7eebe35d8bcc73e89028a1467b162e0c03ff
tuanpv <tuantrylook@gmail.com>
2017-08-07 22:34:45 +0700

    a

--- backend/controllers/BannerController.php
+++ backend/controllers/BannerController.php
@@ -118,7 +118,9 @@ class BannerController extends Controller
                 $file_name = Yii::$app->user->id . '.' . uniqid() . time() . '.' . $image->extension;
                 $tmp = Yii::getAlias('@backend') . '/web/' . Yii::getAlias('@image_banner') . '/';
                 if ($image->saveAs($tmp . $file_name)) {
-                    unlink($tmp . $image_old);
+                    if(file_exists($tmp . $image_old)){
+                        unlink($tmp . $image_old);
+                    }
                     $model->image = $file_name;
                 } else {
                     $model->image = $image_old;
